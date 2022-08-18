59f2e84a4ba9a66da8ef0a34eb431c7c61457089
вашÐKvanarin <kvanarin_dova1997@mail.com>
2022-08-18 09:43:42 +0500

    VueCalculator completed

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,43 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+
+  data () {
+    return {
+      numberOne: 0,
+      numberTwo: 0,
+      operatorActive: '',
+      result: 0,
+    };
+  },
+
+  computed: {
+    sample(a, b) {
+      a = this.numberOne
+      b = this.numberTwo
+      if(this.operatorActive === "sum") {
+        return this.result = (a + b)
+      }
+      if(this.operatorActive === "subtract") {
+        return this.result = (a - b)
+      }
+      if(this.operatorActive === "multiply") {
+        return this.result = (a * b)
+      }
+      if(this.operatorActive === "divide") {
+        return this.result = (a / b)
+      }
+    },
+  },
+  methods: {
+
+  },
+
+});
+
+const app = createApp(App);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
